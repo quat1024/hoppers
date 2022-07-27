b763ba1049c6857c6e468bb5f3e3e4241ea17274
quat1024 <quat1024@users.noreply.github.com>
2022-07-27 04:58:14 -0400

    god damn it's kinda working

---
+++ src/main/java/agency/highlysuspect/hopper/BlockRedstone.java
@@ -0,0 +1,80 @@
+package agency.highlysuspect.hopper;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
+import net.minecraftforge.common.ForgeDirection;
+
+import java.util.Random;
+
+public class BlockRedstone extends Block {
+	public BlockRedstone(int id, Material material) {
+		super(id, material);
+	}
+	
+	//Placing, breaking
+	
+	@Override
+	public void onBlockAdded(World world, int x, int y, int z) {
+		//Lazy impl, double-updates a bunch of blocks but if redstone torches can do it I can too
+		world.notifyBlocksOfNeighborChange(x, y - 1, z, this.blockID);
+		world.notifyBlocksOfNeighborChange(x, y + 1, z, this.blockID);
+		world.notifyBlocksOfNeighborChange(x - 1, y, z, this.blockID);
+		world.notifyBlocksOfNeighborChange(x + 1, y, z, this.blockID);
+		world.notifyBlocksOfNeighborChange(x, y, z - 1, this.blockID);
+		world.notifyBlocksOfNeighborChange(x, y, z + 1, this.blockID);
+	}
+	
+	@Override
+	public void breakBlock(World world, int x, int y, int z, int par5, int par6) {
+		world.notifyBlocksOfNeighborChange(x, y - 1, z, this.blockID);
+		world.notifyBlocksOfNeighborChange(x, y + 1, z, this.blockID);
+		world.notifyBlocksOfNeighborChange(x - 1, y, z, this.blockID);
+		world.notifyBlocksOfNeighborChange(x + 1, y, z, this.blockID);
+		world.notifyBlocksOfNeighborChange(x, y, z - 1, this.blockID);
+		world.notifyBlocksOfNeighborChange(x, y, z + 1, this.blockID);
+	}
+	
+	//Powering
+	
+	@Override
+	public boolean canProvidePower() {
+		return true;
+	}
+	
+	@Override
+	public boolean isProvidingStrongPower(IBlockAccess access, int x, int y, int z, int side) {
+		return false;
+	}
+	
+	@Override
+	public boolean isProvidingWeakPower(IBlockAccess access, int x, int y, int z, int side) {
+		return true;
+	}
+	
+	//yooo.... the forge extension is really this old!!!!!!
+	@Override
+	public boolean canConnectRedstone(IBlockAccess world, int x, int y, int z, int side) {
+		return true;
+	}
+	
+	//Weirdness
+	
+	@Override
+	public boolean isBlockNormalCube(World world, int x, int y, int z) {
+		//This version of the game has a peculiarity with redstone. There are no solid-block power sources!
+		//The only power sources are things like levers, buttons, repeaters, etc. So the only way a normal
+		//cube can provide power is if something else is powering it! isBlockIndirectlyProvidingPowerTo
+		//first checks isBlockNormalCube, and if it returns "true" moves directly on to looking for power
+		//sources adjacent to it instead. Clever...?
+		return false;
+	}
+	
+	@Override
+	public boolean isBlockSolidOnSide(World world, int x, int y, int z, ForgeDirection side) {
+		//So I just said the block isn't a normal cube (for redstone code), but it kinda still is.
+		//I want you to be able to place torches on the side and stuff like that.
+		return true;
+	}
+}
--- src/main/java/agency/highlysuspect/hopper/Hopper.java
+++ src/main/java/agency/highlysuspect/hopper/Hopper.java
@@ -3,7 +3,14 @@ package agency.highlysuspect.hopper;
 import cpw.mods.fml.common.FMLLog;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.registry.LanguageRegistry;
+import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+import net.minecraftforge.common.Configuration;
 
+import java.io.File;
 import java.util.logging.Logger;
 
 @Mod(
@@ -18,11 +25,31 @@ public class Hopper {
 	
 	public static final Logger LOGGER = Logger.getLogger(NAME);
 	
+	public static Configuration config;
+	
+	public static BlockRedstone blockRedstone;
+	
 	@Mod.PreInit
-	public void preinit(FMLPreInitializationEvent e) {
+	public void preinit(FMLPreInitializationEvent pre) {
 		LOGGER.setParent(FMLLog.getLogger());
+		LOGGER.info("Hello world!");
 		
-		LOGGER.info("Hello Ancient World!");
-		LOGGER.warning("Hello Ancient World!");
+		config = new Configuration(new File(pre.getModConfigurationDirectory(), "hopper.conf"));
+		try {
+			config.load();
+			
+			blockRedstone = new BlockRedstone(config.getBlock("hopper-redstoneBlock.id", 730).getInt(), Material.rock);
+			blockRedstone.setBlockName("hopper-redstoneBlock");
+			
+			ItemBlock itemBlockRedstone = new ItemBlock(blockRedstone.blockID - 256);
+			itemBlockRedstone.setCreativeTab(CreativeTabs.tabRedstone);
+			Item.itemsList[blockRedstone.blockID] = itemBlockRedstone;
+			
+		} finally {
+			config.save();
+		}
+		
+		//TODO: Mess with LanguageRegistry.loadLangaugeTable
+		LanguageRegistry.addName(blockRedstone, "Redstone Block");
 	}
 }
